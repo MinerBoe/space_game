dff99bc2231f72d45d9603ce40554749abc46ae7
Mats Dale <14minerboe16@gmail.com>
2017-11-16 12:32:26 -0700

    Add second player

--- window.rb
+++ window.rb
@@ -14,6 +14,9 @@ class Tutorial < Gosu::Window
 		@player = Player.new
 		@player.warp(320, 240)
 
+		@player2 = Player.new
+		@player2.warp(480, 240)
+
 		@star_anim = Gosu::Image.load_tiles("images/star.png", 25, 25)
 		@stars = Array.new
 
@@ -22,31 +25,73 @@ class Tutorial < Gosu::Window
 
 	def update 
 
-		if Gosu.button_down? Gosu::KB_LEFT or Gosu::button_down? Gosu::GP_LEFT
-			@player.turn_left
-		end
+		keystrokes(@player, 1)
 
-		if Gosu.button_down? Gosu::KB_RIGHT or Gosu::button_down? Gosu::GP_RIGHT
-			@player.turn_right
-		end
+		keystrokes(@player2, 2)
 
-		if Gosu.button_down? Gosu::KB_UP or Gosu.button_down? Gosu::GP_BUTTON_0
-			@player.accelerate
-		end
+
+		# if Gosu.button_down? Gosu::KB_LEFT or Gosu::button_down? Gosu::GP_LEFT
+		# 	@player.turn_left
+		# end
+
+		# if Gosu.button_down? Gosu::KB_RIGHT or Gosu::button_down? Gosu::GP_RIGHT
+		# 	@player.turn_right
+		# end
+
+		# if Gosu.button_down? Gosu::KB_UP or Gosu.button_down? Gosu::GP_BUTTON_0
+		# 	@player.accelerate
+		# end
 
 		@player.move
 		@player.collect_stars(@stars)
+
+		@player2.move
+		@player2.collect_stars(@stars)
 
 		if rand(100) < 4 and @stars.size < 25
 			@stars.push(Star.new(@star_anim))
 		end
 	end
 
+	def keystrokes(player, id)
+		if id == 1
+			if Gosu.button_down? Gosu::KB_LEFT or Gosu::button_down? Gosu::GP_LEFT
+				player.turn_left
+			end
+
+			if Gosu.button_down? Gosu::KB_RIGHT or Gosu::button_down? Gosu::GP_RIGHT
+				player.turn_right
+			end
+
+			if Gosu.button_down? Gosu::KB_UP or Gosu.button_down? Gosu::GP_BUTTON_0
+				player.accelerate
+			end
+		end
+
+		if id == 2
+			if Gosu.button_down? Gosu::KB_A
+				player.turn_left
+			end
+
+			if Gosu.button_down? Gosu::KB_D 
+				player.turn_right
+			end
+
+			if Gosu.button_down? Gosu::KB_W or Gosu.button_down? Gosu::GP_BUTTON_0
+				player.accelerate
+			end
+		end
+
+
+	end
+
 	def draw
 		@background_image.draw(0, 0, ZOrder::BACKGROUND)
 		@player.draw
+		@player2.draw
 		@stars.each { |star| star.draw}
-		@font.draw("Score: #{@player.score}", 10, 10, ZOrder::UI, 1.0, 1.0, Gosu::Color::YELLOW)
+		@font.draw("Player 1 Score: #{@player.score}", 10, 10, ZOrder::UI, 1.0, 1.0, Gosu::Color::YELLOW)
+		@font.draw("Player 2 Score: #{@player2.score}", 480, 10, ZOrder::UI, 1.0, 1.0, Gosu::Color::YELLOW)
 	end
 
 	def button_down(id)
